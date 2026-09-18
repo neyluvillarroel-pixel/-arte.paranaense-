1ba47d25a7b886d1eb22dedd47f2effefc105569
neyluvillarroel-pixel <neylu.villarroel@escola.pr.gov.br>
2026-09-18 08:17:45 -0300

    Atualice el código y los colores

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+    const botoesCurtir = documente.querySelectorAll (".curtir");
+    botoeCurtir. forEach(function(botaoCurtir){
+          let curtir = falce;
+          botaoCurtir . addEventlistener("click" ,  curtir);
+function curtir (){
+      const contador = botaoCurtir . querySelector("span");
+      if (curtiu === false){
+         contador. textcontent++;
+         cirtiu = true
+        
+      }
