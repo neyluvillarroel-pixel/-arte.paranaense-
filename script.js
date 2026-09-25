f1812c60163b730672120d4993bc854e39b11333
neyluvillarroel-pixel <neylu.villarroel@escola.pr.gov.br>
2026-09-25 08:32:28 -0300

    atualizei style

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
     botoeCurtir. forEach(function(botaoCurtir){
           let curtir = falce;
           botaoCurtir . addEventlistener("click" ,  curtir);
-function curtir (){
-      const contador = botaoCurtir . querySelector("span");
+function curtir ()}
+     const contador = botaoCurtir . querySelector("span");
       if (curtiu === false){
          contador. textcontent++;
-         cirtiu = true
-        
+         cirtiu = true;
+      }  else{
+            contador.textConten--;
+            curtiu = fase;
       }
+
+      {
+
+      });
